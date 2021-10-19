ac43996e9dbbaf22b4e9e6952e21bf90dc9eaa84
David_Gavilanes <davidgavilanesvelez@gmail.com>
2021-10-19 15:41:34 +0200

    Project scaffolding

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home-test-view', function () {
+    return view('vendor.rw-real-estate.home');
+});
+
+Route::get('/properties-test-view', function () {
+    return view('vendor.rw-real-estate.properties');
+});

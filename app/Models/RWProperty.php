b92d91a7201c4d1230a095dc29adbb5cc66aabe5
David_Gavilanes <davidgavilanesvelez@gmail.com>
2021-10-19 16:28:43 +0200

    Added relations to RWProperty Model

--- app/Models/RWProperty.php
+++ app/Models/RWProperty.php
@@ -6,5 +6,18 @@ use RefineriaWeb\RWRealEstate\Models\Property;
 
 class RWProperty extends Property
 {
-    //
+
+    public $appends = [
+        'property_type'
+    ];
+
+    public function features()
+    {
+        return $this->hasOne(RWPropertyFeature::class, 'property_id', 'id');
+    }
+
+    public function getPropertyTypeAttribute()
+    {
+        return $this->hasOne(RWPropertyType::class, 'id', 'properties_type_id')->first()->type;
+    }
 }
